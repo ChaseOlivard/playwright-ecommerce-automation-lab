2e8ce9205d40e52c214e9c6f32fd8c43f4fb6ba4
Chase Olivard <119266203+ChaseOlivard@users.noreply.github.com>
2026-07-07 16:08:03 -0500

    first custom test login

--- tests/saucedemo-login.spec.ts
+++ tests/saucedemo-login.spec.ts
@@ -7,3 +7,13 @@ test('login page loads' , async ({ page }) => {
     await expect(page.getByPlaceholder('Password')).toBeVisible();
     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
 });
+
+test('successful login shows products page', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page.getByText('Products')).toBeVisible();
+
+});
