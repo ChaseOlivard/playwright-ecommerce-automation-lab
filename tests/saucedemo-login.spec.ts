ab7fbf01adfa83d21ad1635e5dd86a13aaa35214
Chase Olivard <119266203+ChaseOlivard@users.noreply.github.com>
2026-07-08 14:02:58 -0500

    add cart workflow tests

--- tests/saucedemo-login.spec.ts
+++ tests/saucedemo-login.spec.ts
@@ -18,12 +18,47 @@ test('successful login shows products page', async ({ page }) => {
 
 });
 
-test ('invalid login shows error', async ({ page }) => {
+test('invalid login shows error', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_ssauce');
-    await page.getByRole('button', {name: 'Login'}).click();
+    await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
 
 });
+
+test('Successful add to cart', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    const backpackCard = page
+        .locator('[data-test="inventory-item"]')
+        .filter({ hasText: 'Sauce Labs Backpack' });
+    await backpackCard.getByRole('button', { name: 'Add to cart' }).click();
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    await expect(page.getByText('Sauce Labs Backpack')).toBeVisible();
+});
+
+test('Successful remove from cart', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', {name: 'Login'}).click();
+
+    const backpackCard = page
+        .locator('[data-test="inventory-item"]')
+        .filter({ hasText: 'Sauce Labs Backpack' });
+    await backpackCard.getByRole('button', { name: 'Add to cart' }).click();
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    await page.getByRole('button', {name: 'Remove'}).click();
+    await expect(page.locator('[data-test="shopping-cart-badge"]')).not.toBeVisible();
+    await expect(page.getByText('Sauce Labs Backpack')).not.toBeVisible();
+});
+
