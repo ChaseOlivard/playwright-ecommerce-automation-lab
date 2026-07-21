5d4060be3da81fee20c9f597b0e8cf87126b7463
Chase Olivard <119266203+ChaseOlivard@users.noreply.github.com>
2026-07-21 15:22:32 -0500

    added tests for product 1

--- tests/api/products.spec.ts
+++ tests/api/products.spec.ts
@@ -13,15 +13,36 @@ test('GET /products returns a product list', async({ request }) => {
     //: First product checks
     const firstProduct = body.products[0];
     expect(firstProduct.id).toBeDefined();
-    expect(typeof firstProduct.id).toBe('number')
+    expect(typeof firstProduct.id).toBe('number');
 
     expect(firstProduct.title).toBeDefined();
-    expect(typeof firstProduct.title).toBe('string')
+    expect(typeof firstProduct.title).toBe('string');
 
     expect(firstProduct.price).toBeDefined();
-    expect(typeof firstProduct.price).toBe('number')
+    expect(typeof firstProduct.price).toBe('number');
 
     expect(firstProduct.category).toBeDefined();
-    expect(typeof firstProduct.category).toBe('string')
+    expect(typeof firstProduct.category).toBe('string');
 });
+    //: More Checks
+test('GET /products 1 is correct', async({ request }) => {
+    const response = await request.get('https://dummyjson.com/products/1');
+    
+    expect(response.status()).toBe(200);
+    expect(response.headers()['content-type']).toContain('application/json');
+    
+    const body = await response.json();
+    const product = body;
+    expect(product.id).toBeDefined();
+    expect(typeof product.id).toBe('number');
+    expect(product.id).toBe(1);
 
+    expect(product.title).toBeDefined();
+    expect(typeof product.title).toBe('string');
+
+    expect(product.price).toBeDefined();
+    expect(typeof product.price).toBe('number');
+
+    expect(product.category).toBeDefined();
+    expect(typeof product.category).toBe('string'); 
+});
